725e4b6aaccf7d58a3aae0c9c62135137824640d
Siegfried Ehret <siegfried@ehret.me>
2023-05-25 06:57:41 +0200

    🚧 init storyblok migration

--- apps/ehretdotme/astro.config.mjs
+++ apps/ehretdotme/astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from "astro/config";
+import storyblok from "@storyblok/astro";
+import { loadEnv } from "vite";
+
+const env = loadEnv("", process.cwd(), "STORYBLOK");
 
 // https://astro.build/config
 export default defineConfig({
@@ -6,4 +10,19 @@ export default defineConfig({
   server: {
     port: 5173,
   },
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        page: "storyblok/Page",
+        feature: "storyblok/Feature",
+        grid: "storyblok/Grid",
+        teaser: "storyblok/Teaser",
+        Post: "storyblok/Post",
+      },
+      apiOptions: {
+        region: "eu", // optional,  or 'eu' (default)
+      },
+    }),
+  ],
 });
