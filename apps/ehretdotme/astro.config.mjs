aa2f96e4889213c7e7bb9778a7b313612ff41dbf
Siegfried Ehret <siegfried@ehret.me>
2023-08-31 21:23:29 +0200

    🎁 [ehretdotme] add alpine and update museum

--- apps/ehretdotme/astro.config.mjs
+++ apps/ehretdotme/astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
+import alpine from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://ehret.me",
+  integrations: [alpine()],
   server: {
     port: 5173,
   },
