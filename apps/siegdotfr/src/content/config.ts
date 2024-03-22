06ad64b21c443ca5264d407c9127faa1817c768b
Siegfried Ehret <siegfried@ehret.me>
2024-03-22 14:50:11 +0100

    🎁 [siegdotfr] update content config

--- apps/siegdotfr/src/content/config.ts
+++ apps/siegdotfr/src/content/config.ts
@@ -1,38 +1,31 @@
 import { z, defineCollection } from "astro:content";
 
-const badidonSchema = z.object({
-  title: z.string(),
-  description: z.string(),
-  audioUrl: z.string(),
-  audioSize: z.number(),
-  audioDuration: z.number(),
-  date: z.coerce.date().optional(),
-  updatedAt: z.coerce.date().optional(),
-  image: z.string().optional(),
-  tags: z.array(z.string()).optional(),
-});
-
-export type BadidonSchema = z.infer<typeof badidonSchema>;
-
 const badidonCollection = defineCollection({
   type: "content",
-  schema: badidonSchema,
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    audioUrl: z.string(),
+    audioSize: z.number(),
+    audioDuration: z.number(),
+    date: z.coerce.date().optional(),
+    updatedAt: z.coerce.date().optional(),
+    image: z.string().optional(),
+    tags: z.array(z.string()).optional(),
+  }),
 });
-
-const postSchema = z.object({
-  title: z.string(),
-  description: z.string(),
-  date: z.coerce.date().optional(),
-  updatedAt: z.coerce.date().optional(),
-  image: z.string().optional(),
-  tags: z.array(z.string()).optional(),
-});
-
-export type PostSchema = z.infer<typeof postSchema>;
 
 const postCollection = defineCollection({
   type: "content",
-  schema: postSchema,
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    date: z.coerce.date().optional(),
+    updatedAt: z.coerce.date().optional(),
+    image: z.string().optional(),
+    imageDescription: z.string().optional(),
+    tags: z.array(z.string()).optional(),
+  }),
 });
 
 export const collections = {
