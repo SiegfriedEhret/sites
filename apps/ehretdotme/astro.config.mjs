5f5f7bfbf4316c41c44903fef0ee3388ef2cf242
Siegfried Ehret <siegfried@ehret.me>
2024-06-27 07:08:37 +0200

    🎁 add links to headings

--- apps/ehretdotme/astro.config.mjs
+++ apps/ehretdotme/astro.config.mjs
@@ -1,10 +1,18 @@
 import { defineConfig } from "astro/config";
 import alpine from "@astrojs/alpinejs";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://ehret.me",
   integrations: [alpine()],
+  markdown: {
+    rehypePlugins: [
+      rehypeSlug,
+      [rehypeAutolinkHeadings, { behavior: "append" }],
+    ],
+  },
   server: {
     port: 5173,
   },
--- apps/siegdotfr/astro.config.mjs
+++ apps/siegdotfr/astro.config.mjs
@@ -1,11 +1,19 @@
 import { defineConfig } from "astro/config";
 import alpinejs from "@astrojs/alpinejs";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://sieg.fr",
+  integrations: [alpinejs()],
+  markdown: {
+    rehypePlugins: [
+      rehypeSlug,
+      [rehypeAutolinkHeadings, { behavior: "append" }],
+    ],
+  },
   server: {
     port: 5174,
   },
-  integrations: [alpinejs()],
 });
